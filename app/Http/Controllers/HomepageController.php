ed306f5435064595555da3705adcdc145777cc56
Athallah Putra <atha.omniappsolutions@gmail.com>
2023-09-16 08:35:54 +0700

    Added a frontend with features

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller
+{
+    public function index(){
+        return view('index');
+    }
+
+    public function price(){
+        return view('price');
+    }
+
+    public function quote(){
+        return view('quote');
+    }
+
+    public function feature(){
+        return view('feature');
+    }
+    public function contact(){
+            return view('contact');
+    }
+
+    public function about(){
+            return view('about');
+    }
+
+    public function service(){
+        return view('service');
+    }
+    public function team(){
+        return view('team');
+    }
+    public function testimonial(){
+        return view('testimonial');
+    }
+    public function error(){
+        return view('404');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomepageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/home',[HomepageController::class, 'index'])->name('index');
+Route::get('/price',[HomepageController::class, 'price'])->name('price');
+Route::get('/about',[HomepageController::class, 'about'])->name('about');
+Route::get('/contact',[HomepageController::class, 'contact'])->name('contact');
+Route::get('/feature',[HomepageController::class, 'feature'])->name('feature');
+Route::get('/quote',[HomepageController::class, 'quote'])->name('quote');
+Route::get('/service',[HomepageController::class, 'service'])->name('service');
+Route::get('/team',[HomepageController::class, 'team'])->name('team');
+Route::get('/testimonial',[HomepageController::class, 'testimonial'])->name('testimonial');
