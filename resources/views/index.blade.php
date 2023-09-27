f3fa3d441ff2bb48704473e27aca31973673cb1e
Athallah Putra <atha.omniappsolutions@gmail.com>
2023-09-27 20:06:03 +0700

    Added template for reuse purposes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,71 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <title>PTCN Agriculture</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
-
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Roboto:wght@500;700&display=swap"
-        rel="stylesheet">
-
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
-</head>
+@include('template.head')
 
 <body>
     <!-- Spinner Start -->
-    <div id="spinner"
-        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
-            <span class="sr-only">Harap tunggu...</span>
-        </div>
-    </div>
+    @include('template.spinner')
     <!-- Spinner End -->
 
 
     <!-- Navbar Start -->
-    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow border-top border-5 border-primary sticky-top p-0">
-        <a href="{{ route('index') }}" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-            <img src="img/Logo.png" class="logo" height="100" width="200">
-        </a>
-        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="{{ route('index') }}" class="nav-item nav-link active">Home</a>
-                <a href="{{ route('service') }}" class="nav-item nav-link">Kelebihan</a>
-                <a href="{{ route('contact') }}" class="nav-item nav-link">Produk</a>
-                <a href="{{ route('about') }}" class="nav-item nav-link">Tentang Kami</a>
-            </div>
-            <h4 class="m-0 pe-lg-5 d-none d-lg-block"><a href="
-            javascript:void(0)"
-                    onclick="window.open('https://wa.me/6281359225792?text=Halo,',',%20Saya%20tertarik%20dengan%20produk%20pupuk%20anda%20','blank')">
-                    <img width="auto" height="48" src="img/contactus.png" alt="whatsapp--v1" />
-                </a>
-            </h4>
-        </div>
-    </nav>
+    @include('template.navbar')
     <!-- Navbar End -->
 
 
---
+++ resources/views/template/navbar.blade.php
@@ -0,0 +1,24 @@
+<!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg bg-white navbar-light shadow border-top border-5 border-primary sticky-top p-0">
+    <a href="{{ route('index') }}" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <img src="img/Logo.png" class="logo" height="100" width="200">
+    </a>
+    <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto p-4 p-lg-0">
+            <a href="{{ route('index') }}" class="nav-item nav-link active">Home</a>
+            <a href="{{ route('service') }}" class="nav-item nav-link">Kelebihan</a>
+            <a href="{{ route('contact') }}" class="nav-item nav-link">Produk</a>
+            <a href="{{ route('about') }}" class="nav-item nav-link">Tentang Kami</a>
+        </div>
+        <h4 class="m-0 pe-lg-5 d-none d-lg-block"><a href="
+            javascript:void(0)"
+                onclick="window.open('https://wa.me/6281359225792?text=Halo,',',%20Saya%20tertarik%20dengan%20produk%20pupuk%20anda%20','blank')">
+                <img width="auto" height="48" src="img/contactus.png" alt="whatsapp--v1" />
+            </a>
+        </h4>
+    </div>
+</nav>
+<!-- Navbar End -->
