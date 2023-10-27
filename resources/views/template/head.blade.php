99d1f8da9bf4e79b97e295081f2cb02ff02eebd8
Athallah Putra <atha.omniappsolutions@gmail.com>
2023-10-28 03:24:02 +0700

    Changed some Meta-8

--- resources/views/template/head.blade.php
+++ resources/views/template/head.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <title>PTCN Fertilizer</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
+    <meta content="Fertilizers" name="keywords">
+    <meta content="Your #1 Agriculture Fertilizers Solutions" name="description">
 
     <!-- Favicon -->
     <link href="img/favicon.png" rel="icon">
